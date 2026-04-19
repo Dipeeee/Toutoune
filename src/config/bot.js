fa5fa302e7fccdf3c27cff7f6f62081e42cc0a12
Dipee-on <67161656+Dipeeee@users.noreply.github.com>
2026-04-19 20:17:53 +0200

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -30,9 +30,9 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "J'merde les autres patapouf",
+        name: "D'es autres patapouf",
         // Activity type number (0 = Playing).
-        type: 0, 
+        type: 3, 
       },
     ],
   },
